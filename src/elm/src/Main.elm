28fc30086e1d0bcfa7674d4337dacea3df542865
Lukas Turcani <lukasturcani93@gmail.com>
2021-12-16 17:06:38 +0000

    Formatting

--- src/elm/src/Main.elm
+++ src/elm/src/Main.elm
@@ -3,43 +3,57 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html, div)
 
-main = Browser.element
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
 
 
 -- MODEL
+
 
 type alias Model =
     { activeTasks : List ActiveTask
     }
 
-type TimeDelta =
-    TimeDelta Hours Minutes Seconds
+
+type TimeDelta
+    = TimeDelta Hours Minutes Seconds
 
 
-type Hours =
-    Hours Int
-
-type Minutes =
-    Minutes Int
-
-type Seconds =
-    Seconds Int
+type Hours
+    = Hours Int
 
 
-type ActiveTask =
-    ActiveTask
+type Minutes
+    = Minutes Int
+
+
+type Seconds
+    = Seconds Int
+
+
+type ActiveTask
+    = ActiveTask
         { timeTaken : TimeDelta
         }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init flags =
-    ({ a = 12 }, Cmd.none)
+    let
+        model =
+            { activeTasks =
+                [
+                ]
+            }
+    in
+    ( model, Cmd.none )
 
 
 
@@ -47,21 +61,27 @@ init flags =
 
 
 view : Model -> Html Msg
-view model = div [] []
+view model =
+    div [] []
+
 
 
 -- UPDATE
 
-type Msg =
-    Placeholder
 
-update : Msg -> Model -> (Model, Cmd Msg)
+type Msg
+    = Placeholder
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    (model, Cmd.none)
+    ( model, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model
-    = Sub.none
+subscriptions model =
+    Sub.none
